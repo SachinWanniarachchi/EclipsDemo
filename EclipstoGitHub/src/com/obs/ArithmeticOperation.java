4ba912c191209cf37d5fa74869b372a408772686
Sachin Wanniarachchi <Sachin Wanniarachchi@DESKTOP-J9Q2J78>
2020-04-16 09:35:44 +0530

    First Commit

---
+++ EclipstoGitHub/src/com/obs/ArithmeticOperation.java
@@ -0,0 +1,15 @@
+package com.obs;
+
+public class ArithmeticOperation
+{
+
+	public int Addition(int no1,int no2)
+	{
+		return no1+no2;
+	}
+	public int Devided(int no1,int no2)
+	{
+		return no1/no2;
+	}
+
+}
---
+++ EclipstoGitHub/src/com/obs/Main.java
@@ -0,0 +1,12 @@
+package com.obs;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArithmeticOperation AO = new ArithmeticOperation();
+		System.out.print(AO.Addition(15, 22));
+
+	}
+
+}
